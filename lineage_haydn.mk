3dd58576da90c447366d283b25182aae5dc64821
1xtAsh <ashraykrsingh321@gmail.com>
2025-08-02 09:50:05 +0530

    haydn: Bringup LunarisOS

--- lineage_haydn.mk
+++ lineage_haydn.mk
@@ -16,6 +16,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
 PRODUCT_NAME := lineage_haydn
 
+LUNARIS_BUILD_TYPE := UNOFFICIAL
+TARGET_OPTIMIZED_DEXOPT := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="haydn_cn-user 14 UKQ1.231207.002 V816.0.13.0.UKKCNXM release-keys" \
     BuildFingerprint=Xiaomi/haydn_cn/haydn:14/UKQ1.231207.002/V816.0.13.0.UKKCNXM:user/release-keys \

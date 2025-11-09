9704176e603559a66061bca1cac4b1e29780d90c
1xtAsh <ashraykrsingh321@gmail.com>
2025-11-09 17:25:40 +0530

    haydn: miuicamera-haydn

--- device.mk
+++ device.mk
@@ -63,7 +63,7 @@ $(call inherit-product, vendor/xiaomi/haydn/haydn-vendor.mk)
 
 # Call the Leica Camera setup
 $(call soong_config_set,camera,override_format_from_reserved,true)
-$(call inherit-product-if-exists, vendor/xiaomi/haydn-miuicamera/products/miuicamera.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera-haydn/device.mk)
 
 # Sign key
 -include vendor/lineage-priv/keys/keys.mk
